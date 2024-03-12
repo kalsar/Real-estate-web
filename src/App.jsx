7fa2ee7b0092262585beeb563a8fbf26435d8838
Kalsar <alfazkhan560@gmail.com>
2024-03-12 16:59:22 +0530

    successful

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  
+
+  return (
+    <div>
+    <h1 className='text-red-500 '>Hi Kalsar</h1>
+
+    </div>
+  )
+}
+  
+      
+
+
+export default App
