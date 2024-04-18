385f17693e4f67d7ec966bde732223e6efd33428
Kalsar <alfazkhan560@gmail.com>
2024-04-18 16:11:51 +0530

    just correction in code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import Home from './pages/Home'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 
 
@@ -19,7 +20,10 @@ function App() {
     <Route path="/sign-in" element={<SignIn/> } /> 
     <Route path="/sign-up" element={<SignUp/> } /> 
     <Route path="/about" element={<About/>  } /> 
+    <Route element={<PrivateRoute/>}>
+
     <Route path="/profile" element={<Profile/> } /> 
+    </Route>
 
     </Routes>
 
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -3,9 +3,11 @@ import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth';
 import { app } from '../firebase';
 import {useDispatch} from 'react-redux';
 import {signInSuccess} from '../redux/user/userSlice';
+import {useNavigate} from 'react-router-dom';
 
 export default function OAuth() {
   const dispatch=useDispatch();
+  const navigate=useNavigate();
     const handleGoogleClick=async ()=>{
         try{
             const provider=new GoogleAuthProvider();
@@ -17,10 +19,15 @@ export default function OAuth() {
                 'Content-Type':'application/json',
 
               },
-              body:JSON.stringify({name:result.user.displayName, email:result.user.email, photo: result.user.photoURL}),
+              body:JSON.stringify({
+                name: result.user.displayName, 
+                email:result.user.email,
+                 photo: result.user.photoURL,
+                }),
             })
             const data=await res.json()
             dispatch(signInSuccess(data));
+            navigate('/');
 
         }
         catch(error){
